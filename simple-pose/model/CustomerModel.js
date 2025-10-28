0f0c502a95a27cf044e3f1dc1d248350bd901013
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2025-10-28 15:27:43 +0530

    customer model

---
+++ simple-pose/model/CustomerModel.js
@@ -0,0 +1,10 @@
+export default class CustomerModel {
+    constructor(id, fname, lname, email, phone, address) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+}
