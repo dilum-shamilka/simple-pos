5db62520c54b3c5842baa45fd8e34d12bad4dd3c
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2025-10-29 09:20:23 +0530

    load customermethod

---
+++ simple-pose/controller/CustomerController.js
@@ -0,0 +1,29 @@
+import {customer_db} from "../db/db.js";
+import CustomerModel from "../model/CustomerModel.js";
+
+//load customer records
+function loadCustomerTable(){
+
+    $('#customer_table').empty();
+
+    customer_db.map((item, index) => {
+        let fname = item.fname;
+        let lname = item.lname;
+        let email = item.email;
+        let phone = item.phone;
+        let address = item.address;
+
+        let data = `<tr>
+                             <td>${'C' + String(index + 1).padStart(3, '0')}</td> 
+                             <td>${fname}</td>
+                             <td>${lname}</td>
+                             <td>${email}</td>
+                             <td>${phone}</td>
+                             <td>${address}</td>
+                           </tr>`
+
+        $('#customer_table').append(data);
+
+    });
+}
+generateNextId();
