0e49d46181387b25115686c3c51c30b02ffa50bf
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2025-10-29 09:38:47 +0530

    save item

--- simple-pose/controller/ItemController.js
+++ simple-pose/controller/ItemController.js
@@ -36,3 +36,50 @@ function generateNextId() {
     const nextId = 'I' + String(item_db.length + 1).padStart(3, '0');
     $('#item_id').val(nextId);
 }
+
+//save Item
+$('#item_register').on('click', function () {
+    let item_id = $('#item_id').val();
+    let itemName = $('#item_name').val();
+    let category = $('#category').val();
+    let price = parseInt($('#price').val());
+    let qtyInStock = parseInt($('#qty_in_stock').val());
+    let description = $('#description').val();
+
+    if (itemName === '' || category === '' || price === '' || qtyInStock === '' || description ===''){
+        Swal.fire({
+            icon: "error",
+            title: "Error!",
+            text: "Please enter valid inputs..!"
+        });
+    }else{
+        let item_data = {
+            item_id: item_id,
+            itemName: itemName,
+            category: category,
+            price: price,
+            qtyInStock: qtyInStock,
+            description: description
+        };
+
+        item_db.push(item_data);
+        console.log(item_db);
+
+        loadItemTable();
+
+        Swal.fire({
+            title: "Item Added successfully..!",
+            icon: "success",
+            draggable: true
+        });
+    };
+    $('#item_name').val("");
+    $('#category').val("");
+    $('#price').val("");
+    $('#qty_in_stock').val("");
+    $('#description').val("");
+
+    loadItemTable();
+    generateNextId();
+
+});
